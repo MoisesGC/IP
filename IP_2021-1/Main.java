dc4571f8c1d11191bf19f8bbacd5c1edde252d0b
Moises Carvalho <moises@icomp.ufam.edu.br>
2022-03-24 21:34:06 -0400

    Ajuste para aula

--- IP_2021-1/Main.java
+++ IP_2021-1/Main.java
@@ -9,49 +9,14 @@ public class Main{
 		long start, delay;
 		
 		
-		mat1 = new Matriz(4,4);
+		mat1 = new Matriz(10,10);
 		
 		mat1.inicializaOrdenado();
 		mat1.shuffle();
 		mat1.imprime();
 		mat1.ordena();
 		
-		mat1.imprime();
-		
-		
-		//lista1 = new Vetor(20);
-		//lista2 = new Vetor(20);
-		
-		//lista1.inicializaRandomicoV2();
-		//lista2.inicializa(lista1);
-		//lista2.inicializaRandomico();
-		
-		//lista1.imprime();
-		//lista2.imprime();
-		
-		//start = System.nanoTime();
-		//lista1.insertionSort();
-		//delay = System.nanoTime()- start;
-		//System.out.println("Tempo em nano:" + delay);
-		
-		//start = System.nanoTime();
-		//lista2.selectionSort();
-		//delay = System.nanoTime()- start;
-		//System.out.println("Tempo em nano:" + delay);
-		
-		
-		//lista1.imprimeRecursivo();
-		//lista2.imprime();
-		
-		//numFibo = new Fibo();
-		//numFato = new Fatorial();
-		
-		//System.out.println(numFibo.getElemento(10));
-		//numFibo.imprimeSequencia(50);
-		//numFato.imprime(20);
-		
-		//System.out.println();
-		
+		mat1.imprime();	
 				
 	}		
 
