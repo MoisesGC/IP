07acd638833ac835ed7b90cdf342d7d46371579c
Moises Carvalho <moises@icomp.ufam.edu.br>
2023-09-11 19:45:16 -0400

    Aula do objeto Vetor

--- IP_2023-1/aula110923/Main.java
+++ IP_2023-1/aula110923/Main.java
@@ -6,11 +6,41 @@ public class Main{
         
         	System.out.println("Teste do Objeto Vetor");
         	Scanner ler = new Scanner(System.in);
+        	boolean achou;
+		long startTime;
+		long totalTime;
 		
-		Vetor myVet = new Vetor();
+		Vetor myVet = new Vetor(10000);
 		
-		myVet.imprime();
+		//myVet.imprime();
+		/*myVet.setElemento(0,3);
+		myVet.setElemento(1,5);
+		myVet.setElemento(2,8);
+		myVet.setElemento(3,9);
+		myVet.setElemento(4,12);
+		myVet.setElemento(5,12);
+		myVet.setElemento(-3,12);
+		myVet.setElemento(50,12);
+		myVet.getElemento(5);*/
 		
+		
+	
+		myVet.inicializaRandom();
+		achou = myVet.busca(10);
+		
+		startTime = System.nanoTime();
+		achou = myVet.busca(10);
+		totalTime = System.nanoTime() - startTime;
+		System.out.println(achou);
+		System.out.println(totalTime);
+		
+		startTime = System.nanoTime();
+		achou = myVet.buscaV2(10);
+		totalTime = System.nanoTime() - startTime;
+		System.out.println(achou);
+		System.out.println(totalTime);
+		
+		//myVet.imprime();
         
         }
 }
--- IP_2023-1/aula110923/Vetor.java
+++ IP_2023-1/aula110923/Vetor.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Vetor{
 
 	private int[] vet;
@@ -9,6 +11,13 @@ public class Vetor{
 		this.inicializa(-1);
 	}
 	
+	Vetor(int tamInicial){
+		this.setTamanho(tamInicial);
+		vet = new int[this.getTamanho()];
+		this.inicializa(-1);
+	}
+	
+	
 	public int getTamanho(){
 		return this.tamanho;
 	}
@@ -18,11 +27,24 @@ public class Vetor{
 	}
 	
 	public int getElemento(int index){
-		return this.vet[index];
+		int resp;
+		resp = -1;  //retorna -1 no caso de index invalido
+		if((index < this.getTamanho()) && (index >=0)){
+			resp = this.vet[index];
+		}
+		else{
+			System.out.println("Get com index errado!");
+		}	
+		return resp;
 	}
 	
 	public void setElemento(int index, int novoValor){
-		this.vet[index] = novoValor;
+		if((index < this.getTamanho()) && (index >=0)){
+			this.vet[index] = novoValor;
+		}
+		else{
+			System.out.println("Set com index errado!");
+		}	
 	}
 	
 	public void imprime(){
@@ -38,6 +60,43 @@ public class Vetor{
 		for(cont = 0; cont < this.getTamanho(); cont++){	
 			this.setElemento(cont, valor);
 		}
+	}
+	
+	public void inicializaRandom(){
+		int cont;
+		int valor;
+		Random gerador = new Random();
+		for(cont = 0; cont < this.getTamanho(); cont++){
+			valor = gerador.nextInt(this.getTamanho() * 2);	
+			this.setElemento(cont, valor);
+		}
+	}
+	
+	public boolean busca(int chave){
+		boolean achou;
+		int cont;
+		
+		achou = false;
+		for(cont = 0; cont < this.getTamanho(); cont++){	
+			if(chave == this.getElemento(cont)){
+				achou = true;
+			}
+		}		
+		return achou;
+	}
+	
+	public boolean buscaV2(int chave){
+		boolean achou;
+		int cont;		
+		achou = false;
+		cont = 0;
+		while((cont < this.getTamanho()) && !achou){	
+			if(chave == this.getElemento(cont)){
+				achou = true;
+			}
+			cont++;
+		}		
+		return achou;
 	}
 
 }
