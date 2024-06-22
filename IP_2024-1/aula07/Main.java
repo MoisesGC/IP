5e3501b6a3fbdd457253821555bd8b799497605a
Moises Carvalho <moises@icomp.ufam.edu.br>
2024-06-21 20:01:21 -0400

    Aula de Vetores - Operacoes de Algebra

--- IP_2024-1/aula07/Main.java
+++ IP_2024-1/aula07/Main.java
@@ -7,29 +7,59 @@ public class Main{
 		System.out.println("Usando o objeto vetor!");
 		int cont,chave;
 		int chaveInd;
+		float prodInt;
+		float tamanhoVet, similaridade;
 		
 		Scanner entrada = new Scanner(System.in);
 			
-		Vetor listaNumero = new Vetor(5);
+		Vetor vet01 = new Vetor(6);
+		Vetor vet02 = new Vetor(6);
+		Vetor vet03 = new Vetor(6);
 			
-		listaNumero.inicializaManual(entrada);
+			
+		vet01.inicializaManual(entrada);
+		vet02.inicializaManual(entrada);
+					
+		vet01.imprime();
+		vet02.imprime();
 		
-		listaNumero.imprimeEstatisticas();
-		
-		listaNumero.trocaMaiorMenor();	
-		
-		listaNumero.imprime();	
-				
-		/*chave = entrada.nextInt();
-		chaveInd = listaNumero.buscaV3(chave);		
-		if(chaveInd != -1){
-			System.out.println(chave + " estava presente na lista, no endereco "+chaveInd);
-		}
-		else{
-			System.out.println("Nao foi encontrado");
-		}
+		/*prodInt = vet02.produtoInterno();
+		System.out.println("O produto interno dos vetores eh " + prodInt);
+		tamanhoVet = vet02.tamanho();	
+		System.out.println("O tamanho do vetor eh " + tamanhoVet);	
 		*/
 		
+		/*vet03.soma(vet01, vet02, vet03);
+		vet03.imprime();
+		vet02.soma(vet01, vet02);
+		vet02.imprime();
+		vet01.soma(vet01);
+		vet01.imprime();
+		
+		
+		vet03.multiplicaPorEscalar(2.0f);
+		vet03.imprime();	*/
+		
+		
+		/*
+		"eu gosto de bolo cremoso de bolo"
+		"eu gosto de doce cremoso" 
+	        
+		
+		1 - eu
+		2 - gosto
+		3 - de 
+		4 - bolo
+		5 - cremoso
+		6-  doce
+	
+	
+		1,1,2,1,2,1
+		1,1,1,0,1,1 */
+		
+		similaridade = vet03.cosenoEntreVetores(vet01,vet02);
+		System.out.println("A similaridade entre os vetores eh " + similaridade);	
+				
 		entrada.close();	
 	}
 
--- IP_2024-1/aula07/VetorInt.java
+++ IP_2024-1/aula07/VetorInt.java
@@ -3,16 +3,17 @@ import java.util.Random;
 
 public class Vetor{
 
-	private float[] vet;
+	private int[] vet;
 	private int tamanho;
 	
 	Vetor(){
-		this(5);
+		this.setTamanho(5);
+		vet = new int[this.getTamanho()];
 	}
 	
 	Vetor(int novoTamanho){
 		this.setTamanho(novoTamanho);
-		vet = new float[this.getTamanho()];
+		vet = new int[this.getTamanho()];
 	}
 	
 	
@@ -25,16 +26,16 @@ public class Vetor{
 	}
 	
 	
-	public float getElemento(int index){
+	public int getElemento(int index){
 		return this.vet[index];
 	}
 	
-	public void setElemento(int index, float novoValor){
+	public void setElemento(int index, int novoValor){
 		this.vet[index] = novoValor;
-		//this.imprime();
+		this.imprime();
 	}
 	
-	public boolean busca(float chave){
+	public boolean busca(int chave){
 		boolean achou;
 		int cont;
 		achou = false;
@@ -47,7 +48,7 @@ public class Vetor{
 		return achou;
 	}
 	
-	public boolean buscaV2(float chave){
+	public boolean buscaV2(int chave){
 		boolean achou;
 		int cont;
 		achou = false;
@@ -62,7 +63,7 @@ public class Vetor{
 		return achou;
 	}
 	
-	public int buscaV3(float chave){
+	public int buscaV3(int chave){
 		boolean achou;
 		int cont, chaveInd;
 		achou = false;
@@ -81,7 +82,7 @@ public class Vetor{
 	}
 	
 	
-	public void inicializaComValor(float valor){
+	public void inicializaComValor(int valor){
 		int cont;
 		for(cont = 0; cont < this.getTamanho(); cont++){
 			this.setElemento(cont,valor);
@@ -89,26 +90,24 @@ public class Vetor{
 	}
 	
 	public void inicializaManual(Scanner ler){
-		int cont;
-		float entrada;
+		int cont,entrada;
 		for(cont = 0; cont < this.getTamanho(); cont++){
-			entrada = (float)ler.nextInt();
+			entrada = ler.nextInt();
 			this.setElemento(cont,entrada);
 		}
 	}
 	
 	public void inicializaRandomico(){
-		int cont;
-		float entrada;
+		int cont,entrada;
 		Random gerador = new Random();
 		for(cont = 0; cont < this.getTamanho(); cont++){
-			entrada = (float)gerador.nextInt(this.getTamanho() * 2);
+			entrada = gerador.nextInt(this.getTamanho() * 2);
 			this.setElemento(cont,entrada);
 		}
 	}
 	
 	public void troca(int end1, int end2){
-		float valor1,valor2;		
+		int valor1,valor2;		
 		valor2 = this.getElemento(end2);
 		valor1 = this.getElemento(end1);		
 		this.setElemento(end2,valor1);
@@ -117,15 +116,15 @@ public class Vetor{
 	}
 	
 	public void imprimeEstatisticas(){	
-		int cont;
-		float media,maior, menor;	
-		maior = 0.0f;
-		menor = 0.0f;
+		int cont, maior, menor;
+		float media;	
+		maior = 0;
+		menor = 0;
 		media = 0.0f;
 		
 		maior = this.getElemento(0);
 		menor = this.getElemento(0);
-		media = this.getElemento(0);
+		media = (float)this.getElemento(0);
 		
 		
 		for(cont=1; cont < this.getTamanho(); cont++){
@@ -160,95 +159,6 @@ public class Vetor{
 		this.troca(endMaior,endMenor);	
 	}
 	
-	public float produtoInterno(){	
-		return this.produtoInterno(this, this);
-	}
-	
-	public float produtoInterno(Vetor vet1){
-		return this.produtoInterno(vet1, this);
-	}
-	
-	
-	public float produtoInterno(Vetor vet1, Vetor vet2){
-	
-		float acumulador;
-		int cont;
-		
-		acumulador = 0.0f;
-		for(cont = 0; cont < vet1.getTamanho(); cont++){
-			acumulador = acumulador + (vet1.getElemento(cont)* vet2.getElemento(cont));
-		}
-		return acumulador;
-	}
-	
-	public float tamanho(){
-		float prodInt;
-		
-		prodInt = this.produtoInterno(this, this);
-		return (float)Math.sqrt(prodInt);
-	
-	}
-	
-	public void soma(Vetor vet1){
-		this.soma(vet1,this,this);
-	}
-	
-	
-	public void soma(Vetor vet1, Vetor vet2){
-		this.soma(vet1, vet2, this);
-	}
-	
-	public void soma(Vetor vet1, Vetor vet2, Vetor vet3){
-		int cont;
-		float temp;
-		
-		for(cont = 0; cont < vet3.getTamanho(); cont++){
-			temp = vet1.getElemento(cont) + vet2.getElemento(cont);
-			vet3.setElemento(cont, temp);
-		}
-	}
-	
-	public void multiplicaPorEscalar(float escalar){
-		this.multiplicaPorEscalar(escalar, this, this);
-	}
-	
-	
-	public void multiplicaPorEscalar(float escalar, Vetor vet01){
-		this.multiplicaPorEscalar(escalar, vet01, vet01);
-	}
-	
-	
-	public void multiplicaPorEscalar(float escalar, Vetor vet01, Vetor vet02){
-		int cont;
-		float temp;
-		
-		for(cont = 0; cont < vet01.getTamanho(); cont++){
-			temp = vet01.getElemento(cont) * escalar;
-			vet02.setElemento(cont, temp);
-		}
-	}
-	
-	
-	public float cosenoEntreVetores(Vetor vet01, Vetor vet02){
-	
-		float prodEscalar;
-		float multiplicaNormas;
-		
-			
-		prodEscalar = this.produtoInterno(vet01, vet02);
-		System.out.println(prodEscalar);
-		
-		multiplicaNormas = vet01.tamanho() * vet02.tamanho();
-		System.out.println(vet01.tamanho());
-		System.out.println(vet02.tamanho());
-		System.out.println(multiplicaNormas);
-		
-		System.out.println(prodEscalar/multiplicaNormas);
-		
-		return (prodEscalar/multiplicaNormas);
-	
-	
-	}
 	
 	
 	public void imprime(){
@@ -259,7 +169,5 @@ public class Vetor{
 		}
 		System.out.println();
 	}
-	
-
 		
 }	
